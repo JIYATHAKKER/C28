1aa2120f23da5221483843efdfd59ec3c78d65f5
JIYATHAKKER <siddhivinayak.nzb@gmail.com>
2020-06-17 18:54:30 +0530

    added slingshot

--- slingshot.js
+++ slingshot.js
@@ -1,20 +1,28 @@
-class Chain{
-    constructor(bodyA, bodyB){
+class slingshot{
+    constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
-            bodyB: bodyB,
+            pointB: pointB,
             stiffness: 0.04,
             length: 10
         }
         this.chain = Constraint.create(options);
         World.add(world, this.chain);
     }
+    fly(){
+        this.chain.bodyA=null;
+        
+    }
 
     display(){
-        var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
+        if(this.chain.bodyA){
+            var pointA = this.chain.bodyA.position;
+        var pointB = this.chain.pointB;
         strokeWeight(4);
         line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+        
+        
     }
     
 }
